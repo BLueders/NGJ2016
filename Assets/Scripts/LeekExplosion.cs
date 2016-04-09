cb3438e163d356ea775c72c1eab91b28d069f320
Mads Nørgaard Anthony <madsnorgaardanthony@h84.eduroam-2b.wlan.aalborguniversitet.dk>
2016-04-09 17:16:55 +0200

    Fix it to radius

--- Assets/Scripts/LeekExplosion.cs
+++ Assets/Scripts/LeekExplosion.cs
@@ -11,7 +11,7 @@ public class LeekExplosion : MonoBehaviour {
     public float Level;
 
     public void GoBoom(){
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, Level * ExplosionForcePerLevel);
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, Level * ExplosionRadiusPerLevel);
         foreach (Collider2D col in colliders){
             if(col.gameObject.tag == "Player"){
                 col.GetComponent<PlayerHealth>().ReceiveDamage(ExplosionDamagePerLevel * Level);
