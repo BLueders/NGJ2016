6ffc0e5cc6bcffd12453fd564400f45693356025
Mads Nørgaard Anthony <madsnorgaardanthony@h84.eduroam-2b.wlan.aalborguniversitet.dk>
2016-04-09 11:54:51 +0200

    Added a watercan.

---
+++ Assets/Earth.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Earth : MonoBehaviour {
+	int leekLifeTime = 0;
+	public int waterAmount;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(UpdateCr());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//UpdateCr ();
+		//leekLifeTime++;
+	}
+
+	IEnumerator UpdateCr () {
+		while (true) {
+			yield return new WaitForSeconds (3);
+			leekLifeTime++;
+		}
+	}
+}
--- Assets/Scripts/ThrowComponent.cs
+++ Assets/Scripts/ThrowComponent.cs
@@ -7,7 +7,8 @@ public class ThrowComponent : MonoBehaviour {
 	public GameObject throwPivot;
 	public GameObject holdingObject;
 	public bool canThrow = true;
-
+	bool fire1hasBeenPressed;
+	bool fire2hasBeenPressed;
 	public GameObject leek;
 
 	// Use this for initialization
@@ -17,8 +18,28 @@ public class ThrowComponent : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1") && canThrow) {
-			Throw ();
+		if (Input.GetButtonDown ("Fire1") && !fire1hasBeenPressed) {
+			if (canThrow) {
+				Throw ();
+			}
+			fire1hasBeenPressed = true;
+		}
+		if (Input.GetButtonDown ("Fire2") && !fire2hasBeenPressed) {
+			if (!canThrow) {
+				Drop ();
+			}
+			fire2hasBeenPressed = true;
+		}
+
+
+		if (Input.GetButtonUp ("Fire1")) {
+			fire1hasBeenPressed = false;
+		}
+		if (Input.GetButtonUp ("Fire2")) {
+			fire2hasBeenPressed = false;
+		}
+		if (holdingObject != null) {
+			holdingObject.transform.position = holdPivot.transform.position;
 		}
 	}
 
@@ -27,19 +48,39 @@ public class ThrowComponent : MonoBehaviour {
 		holdingObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (5*GetComponent<PlayerMovement>().direction,25);
 		holdingObject.GetComponent<Rigidbody2D> ().isKinematic = false;
 		holdingObject.GetComponent<BoxCollider2D>().isTrigger = false;
+		//holdingObject.transform.parent = transform.parent;
+		holdingObject = null;
+		canThrow = false;
+	}
+
+	void Drop() {
+		if (holdingObject == null) return;
 		holdingObject.transform.parent = transform.parent;
 		holdingObject = null;
 		canThrow = false;
 	}
+
 	void OnTriggerStay2D(Collider2D coll) {
-		if (Input.GetButtonDown ("Fire2") && holdingObject == null) {
+		if (Input.GetButtonDown ("Fire1") && holdingObject == null && !fire1hasBeenPressed) {
 			if (coll.gameObject.tag == "LeekGround") {
 				var inst = (GameObject)(GameObject.Instantiate (leek, throwPivot.transform.position, Quaternion.identity));
-				inst.transform.parent = holdPivot.transform;
-				inst.transform.localPosition = Vector2.zero;
-				inst.transform.localScale = leek.gameObject.transform.localScale;
 				holdingObject = inst;
 				canThrow = true;
+				fire1hasBeenPressed = true;
+			}
+		}
+		if (Input.GetButtonDown ("Fire2") && holdingObject == null && !fire2hasBeenPressed) {
+			if (coll.gameObject.tag == "WaterCan") {
+				holdingObject = coll.gameObject;
+				canThrow = false;
+				fire2hasBeenPressed = true;
+			}
+		}
+		if (coll.gameObject.tag == "LeekGround") {
+			if (Input.GetButtonDown ("Fire2")) {
+				if (holdingObject != null && holdingObject.tag == "WaterCan") {
+					coll.gameObject.GetComponent<Earth>().waterAmount++;
+				}
 			}
 		}
 	}
