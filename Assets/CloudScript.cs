fd4110504a4333a18ba9e219245f0eb8d875cd58
Mads Nørgaard Anthony <madsnorgaardanthony@h84.eduroam-2b.wlan.aalborguniversitet.dk>
2016-04-10 00:20:03 +0200

    added more gfx.

---
+++ Assets/CloudScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudScript : MonoBehaviour {
+	public Vector3 moveV;
+
+	private float minPosX = -15;
+	private float maxPosX = 15;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += moveV*Time.deltaTime;
+		if (transform.position.x>maxPosX) {
+			transform.position = new Vector3 (minPosX+1,transform.position.y,transform.position.z);
+		}
+		if (transform.position.x<minPosX) {
+			transform.position = new Vector3 (maxPosX-1,transform.position.y,transform.position.z);
+		}
+	}
+}
