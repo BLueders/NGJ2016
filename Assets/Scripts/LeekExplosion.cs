ea97d20deb51c6341c57fa38ef05af14e05b09a6
Benno <benno.lueders@gmail.com>
2016-04-09 20:07:58 +0200

    sprite animations for explosion and levelup

--- Assets/Scripts/LeekExplosion.cs
+++ Assets/Scripts/LeekExplosion.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(SpriteAnimator))]
 public class LeekExplosion : MonoBehaviour {
 
     public float ExplosionForcePerLevel;
     public float ExplosionRadiusPerLevel;
     public float ExplosionDamagePerLevel;
     public float Timer = 2;
+    public Sprite[] sprites;
 
     [HideInInspector]
     public float Level;
+
 
     public void Update(){
 		DebugGizmos.DrawWireSphere(transform.position, Level * ExplosionRadiusPerLevel, Color.red);
     }
 
     public void GoBoom(){
+
+        SpriteAnimator animator = GetComponent<SpriteAnimator>();
+        animator.PlayOneShot(GetComponent<SpriteRenderer>(), sprites, 4);
+
 		transform.localScale = transform.localScale * Level * ExplosionRadiusPerLevel;
 
 		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, Level * ExplosionRadiusPerLevel);
--- Assets/Scripts/LeekLevelUpAnimation.cs
+++ Assets/Scripts/LeekLevelUpAnimation.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(SpriteAnimator))]
 public class LeekLevelUpAnimation : MonoBehaviour {
 
+    public Sprite[] sprites;
     public float LiveTime = 5;
-
 	// Use this for initialization
 	void Start () {
-	
+        SpriteAnimator animator = GetComponent<SpriteAnimator>();
+        animator.PlayOneShot(GetComponent<SpriteRenderer>(), sprites, 4);
 	}
 	
 	// Update is called once per frame
