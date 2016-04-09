f0fbd4cc0d94787dfdf077e000de7339aa4a4cbb
Benno <benno.lueders@gmail.com>
2016-04-09 12:04:29 +0200

    Added new InControl framework from blazing badlands Added InputController scripts for player control

---
+++ Assets/Scripts/PlayerCharacterActions.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class PlayerCharacterActions : PlayerActionSet
+{
+    public PlayerAction MoveLeft;
+    public PlayerAction MoveRight;
+    public PlayerAction MoveUp;
+    public PlayerAction MoveDown;
+    public PlayerAction AimLeft;
+    public PlayerAction AimRight;
+    public PlayerAction AimUp;
+    public PlayerAction AimDown;
+    public PlayerTwoAxisAction Aim;
+    public PlayerTwoAxisAction Move;
+    public PlayerAction Jump;
+    public PlayerAction Crouch;
+    public PlayerAction Walk;
+    public PlayerAction Dash;
+    public PlayerAction PrimaryAttack;
+    public PlayerAction SecondaryAttack;
+
+    public PlayerCharacterActions()
+    {
+        // Movement
+        MoveLeft = CreatePlayerAction("Move Left");
+        MoveRight = CreatePlayerAction("Move Right");
+        MoveUp = CreatePlayerAction("Move Up");
+        MoveDown = CreatePlayerAction("Move Down");
+        Move = CreateTwoAxisPlayerAction(MoveLeft, MoveRight, MoveDown, MoveUp);
+
+        // Aiming
+        AimLeft = CreatePlayerAction("Aim Left");
+        AimRight = CreatePlayerAction("Aim Right");
+        AimUp = CreatePlayerAction("Aim Up");
+        AimDown = CreatePlayerAction("Aim Down");
+        Aim = CreateTwoAxisPlayerAction(AimLeft, AimRight, AimDown, AimUp);
+
+        // Actions
+        Jump = CreatePlayerAction("Jump");
+        Crouch = CreatePlayerAction("Crouch");
+        Walk = CreatePlayerAction("Walk");
+        Dash = CreatePlayerAction("Dash");
+        PrimaryAttack = CreatePlayerAction("Primary Attack");
+        SecondaryAttack = CreatePlayerAction("Secondary Attack");
+    }
+
+    public static PlayerCharacterActions GetDefaultBindings(bool includeKeyboardActions, bool includeControllerActions)
+    {
+        var actions = new PlayerCharacterActions();
+
+        // Keyboard actions
+        if(includeKeyboardActions)
+        {
+            AddDefaultKeyboardBindings(actions);
+        }
+
+        // Controller actions
+        if(includeControllerActions)
+        {
+            AddDefaultControllerBindings(actions);
+        }
+
+        return actions;
+    }
+
+    private static void AddDefaultKeyboardBindings(PlayerCharacterActions actions)
+    {
+        // Movement
+        actions.MoveLeft.AddDefaultBinding( Key.A );
+        actions.MoveLeft.AddDefaultBinding( Key.LeftArrow );
+        actions.MoveRight.AddDefaultBinding( Key.D );
+        actions.MoveRight.AddDefaultBinding( Key.RightArrow );
+        actions.MoveUp.AddDefaultBinding( Key.W );
+        actions.MoveUp.AddDefaultBinding( Key.UpArrow );
+        actions.MoveDown.AddDefaultBinding( Key.S );
+        actions.MoveDown.AddDefaultBinding( Key.DownArrow );
+        
+        // Aiming
+        // TODO: Add mouse aiming using custom source
+
+        // Actions
+        actions.Jump.AddDefaultBinding( Key.Space );
+        actions.Crouch.AddDefaultBinding( Key.C );
+        actions.Crouch.AddDefaultBinding( Key.LeftControl );
+        actions.Walk.AddDefaultBinding( Key.LeftShift );
+        actions.Dash.AddDefaultBinding( Mouse.MiddleButton );
+        actions.PrimaryAttack.AddDefaultBinding( Mouse.LeftButton );
+        actions.SecondaryAttack.AddDefaultBinding( Mouse.RightButton );
+    }
+
+    private static void AddDefaultControllerBindings(PlayerCharacterActions actions)
+    {
+        // Movement
+        actions.MoveLeft.AddDefaultBinding( InputControlType.DPadLeft );
+        actions.MoveLeft.AddDefaultBinding( InputControlType.LeftStickLeft );
+        actions.MoveRight.AddDefaultBinding( InputControlType.DPadRight );
+        actions.MoveRight.AddDefaultBinding( InputControlType.LeftStickRight );
+        actions.MoveUp.AddDefaultBinding( InputControlType.DPadUp );
+        actions.MoveUp.AddDefaultBinding( InputControlType.LeftStickUp );
+        actions.MoveDown.AddDefaultBinding( InputControlType.DPadDown );
+        actions.MoveDown.AddDefaultBinding( InputControlType.LeftStickDown );
+        
+        // Aiming
+        actions.AimLeft.AddDefaultBinding( InputControlType.RightStickLeft );
+        actions.AimRight.AddDefaultBinding( InputControlType.RightStickRight );
+        actions.AimUp.AddDefaultBinding( InputControlType.RightStickUp );
+        actions.AimDown.AddDefaultBinding( InputControlType.RightStickDown );
+        
+        // Actions
+        actions.Jump.AddDefaultBinding( InputControlType.Action1 );
+        actions.Crouch.AddDefaultBinding( InputControlType.Action2 );
+        actions.Walk.AddDefaultBinding( InputControlType.Action3 );
+        actions.Dash.AddDefaultBinding( InputControlType.Action4 );
+        actions.PrimaryAttack.AddDefaultBinding( InputControlType.LeftBumper );
+        actions.SecondaryAttack.AddDefaultBinding( InputControlType.RightBumper );
+    }
+
+    // TODO: Implement custom source, reading the mouse position on screen and calculating a vector from the player to the mouse (see: https://www.youtube.com/watch?v=5wnQ9hSBB0I)
+}
---
+++ Assets/Scripts/PlayerCharacterInputController.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(ThrowComponent))]
+[RequireComponent(typeof(PlayerHealth))]
+public class PlayerCharacterInputController : MonoBehaviour
+{
+    public PlayerMovement playerMovement { get; private set; }
+    public ThrowComponent throwComponent { get; private set; }
+    public PlayerHealth playerHealth { get; private set; }
+
+    public PlayerCharacterActions playerCharacterActions { get; private set; }
+    public Transform aimPivot;
+    Vector3 moveDirection;
+    bool mouseEnabled;
+    bool applicationHasFocus = true;
+
+    void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        throwComponent = GetComponent<ThrowComponent>();
+    }
+
+    void Start()
+    {
+    }
+    
+    void FixedUpdate()
+    {
+        if(playerHealth.IsAlive()) // Only allow input when alive
+        {
+            HandleMove();
+        }
+    }
+
+    void Update()
+    {
+        if(playerHealth.IsAlive()) // Only allow input when alive
+        {
+            //HandleAim();
+            HandleAttack();
+        }
+    }
+
+    void HandleMove()
+    {
+        // Read inputs
+        float x = playerCharacterActions.Move.X;
+        float y = playerCharacterActions.Move.Y;
+        bool jump = playerCharacterActions.Jump.IsPressed;
+        bool walk = playerCharacterActions.Walk.IsPressed;
+
+        playerMovement.Move(x,y,jump);
+    }
+    /*
+    void HandleAim()
+    {
+        // Read inputs
+        Vector2 aimDirection = mouseEnabled ? GetMouseAimDirection() : playerCharacterActions.Aim.Value;
+
+        // Update aim pivot if able
+        if (aimPivot != null)
+        {
+            if (aimDirection != Vector2.zero) // If aiming using stick
+            {
+                var aimAngle = MathHelper.VectorToAngle(aimDirection);
+                
+                // HACK: Offsetting stick aim angle by 90 degrees to fix incorrect aim angle
+                aimPivot.rotation = Quaternion.Euler(0, -aimAngle + 90, 0);
+            }
+            else // If not aiming using stick
+            {
+                // Use character facing direction for aim
+                aimPivot.localRotation = Quaternion.identity;
+            }
+        }
+    }
+    */
+    void HandleAttack()
+    {
+        // Read inputs
+        bool primaryAttack = playerCharacterActions.PrimaryAttack.IsPressed;
+        bool secondaryAttack = playerCharacterActions.SecondaryAttack.IsPressed;
+
+        // Primary attack takes precedence
+        if(primaryAttack)
+        {
+            // Attack if able
+            weaponInventory.GetEquippedWeapon().UsePrimaryAttack(aimPivot.position, aimPivot.forward);
+        }
+        else if(secondaryAttack)
+        {
+            // Attack if able
+            weaponInventory.GetEquippedWeapon().UseSecondaryAttack(aimPivot.position, aimPivot.forward);
+        }
+    }
+
+    public void SetupPlayerActions(bool includeKeyboardActions, bool includeControllerActions)
+    {
+        playerCharacterActions = PlayerCharacterActions.GetDefaultBindings(includeKeyboardActions, includeControllerActions);
+        mouseEnabled = includeKeyboardActions;
+    }
+
+    void OnApplicationFocus(bool hasFocus)
+    {
+        applicationHasFocus = hasFocus;
+    }
+
+    Vector2 GetMouseAimDirection()
+    {
+        Vector2 aimDirection = Vector2.zero;
+
+        if(applicationHasFocus)
+        {
+            Vector3 aimPointWorldSpace = Vector3.zero;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            float maxDistance = 100f;
+            
+            if (Physics.Raycast(ray, out hit, maxDistance))
+            {
+                aimPointWorldSpace = hit.point;
+            }
+            
+            Vector3 deltaPosition = aimPointWorldSpace - transform.position;
+            aimDirection.Set(deltaPosition.x, deltaPosition.z);
+            aimDirection.Normalize();
+        }
+
+        return aimDirection;
+    }
+}
